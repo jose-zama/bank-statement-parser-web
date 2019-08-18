c5c1fa0d32ee31fd5d319887671d92b4cf7228cd
Zamudio, Jose <jose.zamudio@thermofisher.com>
2019-08-18 16:45:07 -0700

    Add some styles, and more...

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-import * as wasm from "hello-wasm-pack";
+import * as bank_reader from "bank-statement-reader";
 
-wasm.greet();
+//var balance = document.getElementById("balance").innerHTML;
+
+function parse(xml){
+	
+	// console.log(xml);
+
+	bank_reader.parse_statement(xml);
+	
+	
+	// balance = bank_reader.get_balance(xml);
+	// console.log(balance);
+	// document.getElementById("balance").innerHTML = balance;
+}
+
+// document.getElementById("textarea").onchange = function(){
+// 	let xml = document.getElementById("textarea").value;
+// 	parse(xml);
+// }
+
+window.parse = function(){
+	var input = document.querySelector('input');
+	let curFiles = input.files;
+	if(curFiles.length !== 0) {
+		let file = input.files[0];
+		//console.log(file);
+		var r = new FileReader();
+		r.onload = function(e) {
+				parse(e.target.result);
+			};
+		r.readAsText(file)		
+	}
+	
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,47 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: {
+    bootstrap: './bootstrap.js',
+    styles: './app.scss'
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "[name].js",
   },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+			  loader: 'sass-loader',
+			  options: {
+			    includePaths: ['./node_modules']
+			  }
+		},
+        ]
+      },
+      {
+        test: /\.html$/,
+        options: {
+          includePaths: ['./node_modules/bower_components/highcharts-chart']
+        },
+        loader: 'polymer-webpack-loader'
+      },
+    ]
+  },
 };
